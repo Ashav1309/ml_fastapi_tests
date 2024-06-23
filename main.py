0812b8fe6b74f54e15f94671198c3edff6ad6378
Ashav1309 <ashav1309@gmail.com>
2024-06-23 18:58:44 +0500

    com

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
 
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
 
 class Item(BaseModel):
     text: str
 
-
-
-app = FastAPI()
-classifier = pipeline("sentiment-analysis")
-
-
-
+# Корневой маршрут
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
+# Функция для получения экземпляра классификатора
+def get_classifier():
+    return classifier
 
+# Маршрут для предсказания сентимента текста
 @app.post("/predict/")
-def predict(item: Item):
-    return classifier(item.text)[0]
+def predict(item: Item, classifier = Depends(get_classifier)):
+    try:
+        result = classifier(item.text)[0]  # Получаем результат от классификатора
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")  # Обработка ошибки в случае возникновения исключения
